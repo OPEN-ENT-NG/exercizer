26097e9310d599b381484e05d305eea7d7b0a67e
Erwan Le Poder <e.lepoder@thecodingmachine.com>
2016-05-03 10:15:36 +0200

    copy service

--- src/main/resources/public/app/services/grainCopyService.ts
+++ src/main/resources/public/app/services/grainCopyService.ts
@@ -1,13 +1,13 @@
 /**
  * Created by jun on 22/04/2016.
  */
-interface ICopyService {
-    getCopyList(params, callbackSuccess, callbackFail);
-    copyList : ISubjectCopy[];
-    isSetCopyList : boolean;
+interface IGrainCopyService {
+    getGrainCopyListBySubjectCopyId(subject_copy_id, callbackSuccess, callbackFail);
+    grainCopyList : ISubjectGrainCopy[];
+    isSetGrainCopyList : boolean;
 }
 
-class CopyService implements ICopyService {
+class GrainCopyService implements IGrainCopyService {
 
     static $inject = [
         'serverUrl',
@@ -17,8 +17,8 @@ class CopyService implements ICopyService {
     private serverUrl : string;
     private $http : any;
 
-    private _copyList :ISubjectCopy[];
-    private _isSetCopyList : boolean;
+    private _grainCopyList :ISubjectGrainCopy[];
+    private _isSetGrainCopyList : boolean;
 
 
     constructor(
@@ -28,46 +28,46 @@ class CopyService implements ICopyService {
         this.serverUrl = serverUrl;
         this.$http = $http;
 
-        this._isSetCopyList = false;
-        this._copyList = [];
+        this._isSetGrainCopyList = false;
+        this._grainCopyList = [];
 
     }
 
 
-    public get copyList():ISubjectCopy[] {
-        return this._copyList;
+    public get grainCopyList():ISubjectGrainCopy[] {
+        return this._grainCopyList;
     }
 
 
-    public get isSetCopyList():boolean {
-        return this._isSetCopyList;
+    public get isSetGrainCopyList():boolean {
+        return this._isSetGrainCopyList;
     }
 
     /**
-     * Set isSetCopyList is used to force refresh copy list
+     * Set isSetGrainCopyList is used to force refresh grainCopy list
      * @param value
      */
-    public set isSetCopyList(value:boolean) {
-        this._isSetCopyList = value;
+    public set isSetGrainCopyList(value:boolean) {
+        this._isSetGrainCopyList = value;
     }
 
 
-    private addCopyToCopyList(copy : ISubjectCopy){
-        if(this._copyList[copy.id]){
+    private addGrainCopyToGrainCopyList(grainCopy : ISubjectGrainCopy){
+        if(this._grainCopyList[grainCopy.id]){
             // overwrite
         }
-        this._copyList[copy.id] = copy;
+        this._grainCopyList[grainCopy.id] = grainCopy;
     }
 
-    public getCopyList(params, callbackSuccess, callbackFail){
+    public getGrainCopyList(params, callbackSuccess, callbackFail){
         var self = this;
-        if(this._isSetCopyList){
-            callbackSuccess(this._copyList)
+        if(this._isSetGrainCopyList){
+            callbackSuccess(this._grainCopyList)
         } else{
-            this._getCopyList(params,
+            this._getGrainCopyList(params,
                 function(data){
-                    self._copyList = data;
-                    self._isSetCopyList = true;
+                    self._grainCopyList = data;
+                    self._isSetGrainCopyList = true;
                     callbackSuccess(data);
                 },
                 callbackFail()
@@ -75,7 +75,7 @@ class CopyService implements ICopyService {
         }
     }
 
-    private _getCopyList(params, callbackSuccess, callbackFail){
+    private _getGrainCopyList(params, callbackSuccess, callbackFail){
         var req: any;
         var self = this;
         req = this.$http({
@@ -89,7 +89,7 @@ class CopyService implements ICopyService {
         req
             .success(function (data, status, headers, config) {
                 if (status == 200) {
-                    // DATA  : list of copy
+                    // DATA  : list of grainCopy
                     callbackSuccess(data);
                 } else{
                     callbackFail(data);
