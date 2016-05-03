6603a285616c31a628119f7066277bfe8371d217
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-05-03 10:54:08 +0200

    fixing structutre and some stuff

--- src/main/resources/public/app/directives/subjectOrganizer.ts
+++ src/main/resources/public/app/directives/subjectOrganizer.ts
@@ -1,9 +1,13 @@
-/// <reference path="./../typings/angular-1.2.d.ts"/>
-var subjectOrganizer = [ () => {
-    return {
-        restrict: "E",
-        templateUrl: 'exercizer/public/app/templates/directives/subjectOrganizer.html',
-        link:(scope : any, element, attrs) => {
-        }
-    };
-}];
+directives.push(
+    {
+        name: "subjectOrganizer",
+        injections: [ () => {
+            return {
+                restrict: "E",
+                templateUrl: 'exercizer/public/app/templates/directives/subjectOrganizer.html',
+                link:(scope : any, element, attrs) => {
+                }
+            };
+        }]
+    }
+);
--- src/main/resources/public/app/directives/subjectsTree.ts
+++ src/main/resources/public/app/directives/subjectsTree.ts
@@ -1,9 +1,13 @@
-/// <reference path="./../typings/angular-1.2.d.ts"/>
-var subjectsTree = [ () => {
-    return {
-        restrict: "E",
-        templateUrl: 'exercizer/public/app/templates/directives/subjectsTree.html',
-        link:(scope : any, element, attrs) => {
-        }
-    };
-}];
+directives.push(
+    {
+        name: "subjectsTree",
+        injections: [ () => {
+            return {
+                restrict: "E",
+                templateUrl: 'exercizer/public/app/templates/directives/subjectsTree.html',
+                link:(scope : any, element, attrs) => {
+                }
+            };
+        }]
+    }
+);
