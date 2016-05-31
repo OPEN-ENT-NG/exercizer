c7f3f52aeebd9cad7b2d9f9e3fd0b8437d8ff455
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-05-31 12:35:56 +0200

    adding missing controller

--- src/main/java/fr/openent/exercizer/Exercizer.java
+++ src/main/java/fr/openent/exercizer/Exercizer.java
@@ -1,5 +1,6 @@
 package fr.openent.exercizer;
 
+import fr.openent.exercizer.controllers.ExercizerController;
 import fr.openent.exercizer.controllers.FolderController;
 import fr.openent.exercizer.controllers.GrainController;
 import fr.openent.exercizer.controllers.SubjectController;
@@ -11,6 +12,7 @@ public class Exercizer extends BaseServer {
     @Override
     public void start() {
         super.start();
+        addController(new ExercizerController());
         addController(new FolderController());
         addController(new SubjectController());
         addController(new GrainController());
