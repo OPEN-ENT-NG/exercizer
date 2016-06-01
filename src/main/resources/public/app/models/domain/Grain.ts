6dfa49a2cf44ad01e15d630f258c659e1193eab5
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-06-01 17:38:55 +0200

    refactoring some models for backend purpose

--- src/main/resources/public/app/models/domain/Grain.ts
+++ src/main/resources/public/app/models/domain/Grain.ts
@@ -10,13 +10,13 @@ interface IGrain {
 
 class Grain implements IGrain {
 
-    private _id:number;
-    private _subject_id:number;
-    private _grain_type_id:number;
-    private _created:string;
-    private _modified:string;
-    private _order_by:number;
-    private _grain_data:IGrainData;
+    id:number;
+    subject_id:number;
+    grain_type_id:number;
+    created:string;
+    modified:string;
+    order_by:number;
+    grain_data:IGrainData;
 
     constructor
     (
@@ -27,70 +27,14 @@ class Grain implements IGrain {
         modified?:string,
         order_by?:number,
         grain_data?:IGrainData
-    )
+    ) 
     {
-        this._id = id;
-        this._subject_id = subject_id;
-        this._grain_type_id = grain_type_id;
-        this._created = created;
-        this._modified = modified;
-        this._order_by = order_by;
-        this._grain_data = grain_data;
-    }
-
-    get id():number {
-        return this._id;
-    }
-
-    set id(value:number) {
-        this._id = value;
-    }
-
-    get subject_id():number {
-        return this._subject_id;
-    }
-
-    set subject_id(value:number) {
-        this._subject_id = value;
-    }
-
-    get grain_type_id():number {
-        return this._grain_type_id;
-    }
-
-    set grain_type_id(value:number) {
-        this._grain_type_id = value;
-    }
-
-    get created():string {
-        return this._created;
-    }
-
-    set created(value:string) {
-        this._created = value;
-    }
-
-    get modified():string {
-        return this._modified;
-    }
-
-    set modified(value:string) {
-        this._modified = value;
-    }
-
-    get order_by():number {
-        return this._order_by;
-    }
-
-    set order_by(value:number) {
-        this._order_by = value;
-    }
-
-    get grain_data():IGrainData {
-        return this._grain_data;
-    }
-
-    set grain_data(value:IGrainData) {
-        this._grain_data = value;
+        this.id = id;
+        this.subject_id = subject_id;
+        this.grain_type_id = grain_type_id;
+        this.created = created;
+        this.modified = modified;
+        this.order_by = order_by;
+        this.grain_data = grain_data;
     }
 }
--- src/main/resources/public/app/models/domain/GrainDocument.ts
+++ src/main/resources/public/app/models/domain/GrainDocument.ts
@@ -10,13 +10,13 @@ interface IGrainDocument {
 
 class GrainDocument implements IGrainDocument {
 
-    private _id:number;
-    private _owner:string;
-    private _ownerName:string;
-    private _created:string;
-    private _title:string;
-    private _name:string;
-    private _path:string;
+    id:number;
+    owner:string;
+    ownerName:string;
+    created:string;
+    title:string;
+    name:string;
+    path:string;
 
     constructor
     (
@@ -29,68 +29,12 @@ class GrainDocument implements IGrainDocument {
         path?:string
     )
     {
-        this._id = id;
-        this._owner = owner;
-        this._ownerName = ownerName;
-        this._created = created;
-        this._title = title;
-        this._name = name;
-        this._path = path;
-    }
-
-    get id():number {
-        return this._id;
-    }
-
-    set id(value:number) {
-        this._id = value;
-    }
-
-    get owner():string {
-        return this._owner;
-    }
-
-    set owner(value:string) {
-        this._owner = value;
-    }
-
-    get ownerName():string {
-        return this._ownerName;
-    }
-
-    set ownerName(value:string) {
-        this._ownerName = value;
-    }
-
-    get created():string {
-        return this._created;
-    }
-
-    set created(value:string) {
-        this._created = value;
-    }
-
-    get title():string {
-        return this._title;
-    }
-
-    set title(value:string) {
-        this._title = value;
-    }
-
-    get name():string {
-        return this._name;
-    }
-
-    set name(value:string) {
-        this._name = value;
-    }
-
-    get path():string {
-        return this._path;
-    }
-
-    set path(value:string) {
-        this._path = value;
+        this.id = id;
+        this.owner = owner;
+        this.ownerName = ownerName;
+        this.created = created;
+        this.title = title;
+        this.name = name;
+        this.path = path;
     }
 }
