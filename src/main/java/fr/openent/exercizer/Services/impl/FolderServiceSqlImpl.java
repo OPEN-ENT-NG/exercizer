efbb3e7dd2a06d6234a50b16a519000efa6430ab
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-06-01 17:42:42 +0200

    adding backend java files

---
+++ src/main/java/fr/openent/exercizer/Services/impl/FolderServiceSqlImpl.java
@@ -0,0 +1,47 @@
+package fr.openent.exercizer.services.impl;
+
+import fr.openent.exercizer.services.IFolderService;
+import fr.wseduc.webutils.Either;
+import org.entcore.common.user.UserInfos;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+
+public class FolderServiceSqlImpl extends AbstractExercizerServiceSqlImpl implements IFolderService {
+
+    public FolderServiceSqlImpl() {
+        super("exercizer", "folder");
+    }
+
+    /**
+     * @see fr.openent.exercizer.services.impl.AbstractExercizerServiceSqlImpl
+     */
+    @Override
+    public void persist(final JsonObject resource, final UserInfos user, final Handler<Either<String, JsonObject>> handler) {
+        super.persist(resource, true, user, handler);
+    }
+
+    /**
+     * @see fr.openent.exercizer.services.impl.AbstractExercizerServiceSqlImpl
+     */
+    @Override
+    public void update(final JsonObject resource, final UserInfos user, final Handler<Either<String, JsonObject>> handler) {
+        super.update(resource, user, handler);
+    }
+
+    /**
+     * @see fr.openent.exercizer.services.impl.AbstractExercizerServiceSqlImpl
+     */
+    @Override
+    public void remove(final JsonObject resource, final UserInfos user, final Handler<Either<String, JsonObject>> handler) {
+        super.delete(resource, user, handler);
+    }
+
+    /**
+     * @see fr.openent.exercizer.services.impl.AbstractExercizerServiceSqlImpl
+     */
+    @Override
+    public void list(final UserInfos user, final Handler<Either<String, JsonArray>> handler) {
+        super.list(user, handler);
+    }
+}
