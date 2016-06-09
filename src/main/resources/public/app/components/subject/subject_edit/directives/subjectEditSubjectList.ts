24d0623d55608370ebda7f0faa120768de045d6b
Erwan Le Poder <e.lepoder@thecodingmachine.com>
2016-06-09 18:54:27 +0200

    edit subject subject list

--- src/main/resources/public/app/components/subject/subject_edit/directives/subjectEditSubjectList.ts
+++ src/main/resources/public/app/components/subject/subject_edit/directives/subjectEditSubjectList.ts
@@ -2,8 +2,8 @@ directives.push(
     {
         name: 'subjectEditSubjectList',
         injections: [
-            'FolderService',
-            (FolderService
+            'FolderService','SubjectService','GrainService',
+            (FolderService, SubjectService, GrainService
              ) => {
                 return {
                     restrict: 'E',
@@ -12,9 +12,64 @@ directives.push(
                     templateUrl: 'exercizer/public/app/components/subject/subject_edit/templates/subject-edit-subject-list.html',
                     link: (scope:any) => {
 
-                        scope.folderList = function () {
-                            return FolderService.folderList;
+                        scope.autocomplete = {
+                            subjectList: null
                         };
+
+                        scope.grainList = [];
+
+                        /**
+                         * GETTER
+                         */
+
+                        scope.subjectList = function () {
+                            return SubjectService.getList();
+                        };
+
+                        /**
+                         * AUTOCOMPLETE
+                         */
+
+                        scope.clickOnAutoComplete = function () {
+                            if (scope.subjectList()) {
+                                scope.autocomplete.subjectList = createListAutoComplete();
+                            }
+                        };
+
+                        scope.clickOnItem = function(subjectFromAutoComplete){
+                            scope.subject = SubjectService.getById(subjectFromAutoComplete.id);
+                            GrainService.getListBySubject(scope.subject).then(
+                                function(data){
+                                    scope.grainList = data;
+                                }
+                            )
+
+                        };
+
+                        function createListAutoComplete() {
+                            var array = [];
+                            angular.forEach(scope.subjectList(), function (value) {
+                                var folder = null,
+                                    folderString = "";
+                                if (value.folder_id) {
+                                    folder = FolderService.folderById(value.folder_id);
+                                    if (folder) {
+                                        folderString = " (" + folder.label + ")";
+                                    }
+                                }
+                                var obj = {
+                                    title: value.title,
+                                    name: value.title + folderString,
+                                    id: value.id,
+                                    toString: function () {
+                                        return this.name;
+                                    }
+                                };
+                                array.push(obj);
+                            });
+                            return array;
+                        }
+
 
                     }
                 };
