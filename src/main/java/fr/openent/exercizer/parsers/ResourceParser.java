188a8080becede8a5bb6273d6d8adb9de7f8c6d3
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-06-10 15:23:33 +0200

    adding has_automatic_display and remove fields myRights

--- src/main/java/fr/openent/exercizer/parsers/ResourceParser.java
+++ src/main/java/fr/openent/exercizer/parsers/ResourceParser.java
@@ -11,6 +11,7 @@ public class ResourceParser {
 		fields.add("index");
 		fields.add("selected");
 		fields.add("shared");
+		fields.add("myRights");
 		fields.add("created");
 		fields.add("modified");
 		
