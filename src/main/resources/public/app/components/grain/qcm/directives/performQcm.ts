d6db7f479bbc6380120009977ae2a4f191746039
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-06-10 17:02:04 +0200

    redesigning order answer edit

--- src/main/resources/public/app/components/grain/qcm/directives/performQcm.ts
+++ src/main/resources/public/app/components/grain/qcm/directives/performQcm.ts
@@ -1,25 +1,22 @@
 directives.push(
     {
-        name: 'qcmPerform',
-        injections: [
-            (
-            ) => {
-                return {
-                    restrict: 'E',
-                    scope: {
-                        grainCopy: '='
-                    },
-                    templateUrl: 'exercizer/public/app/components/grain/qcm/templates/perform-qcm.html',
-                    link:(scope:any) => {
+        name: 'performQcm',
+        injections: [() => {
+            return {
+                restrict: 'E',
+                scope: {
+                    grainCopy: '='
+                },
+                templateUrl: 'exercizer/public/app/components/grain/qcm/templates/perform-qcm.html',
+                link:(scope:any) => {
 
-                        scope.updateGrainCopy = function() {
-                            scope.$emit("E_UPDATE_GRAIN_COPY", scope.grainCopy);
-                        };
+                    scope.updateGrainCopy = function() {
+                        scope.$emit('E_UPDATE_GRAIN_COPY', scope.grainCopy);
+                    };
 
-                    }
-                };
-            }
-        ]
+                }
+            };
+        }]
     }
 );
 
