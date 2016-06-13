981e28177a21259d7c4226433e9f49b8a24d8c30
Erwan Le Poder <e.lepoder@thecodingmachine.com>
2016-06-13 11:39:25 +0200

    get scheduled sibject student part

--- src/main/resources/public/app/services/SubjectScheduledService.ts
+++ src/main/resources/public/app/services/SubjectScheduledService.ts
@@ -32,9 +32,6 @@ class SubjectScheduledService implements ISubjectScheduledService {
         this._$http = _$http;
         this._grainService = _grainService;
         this._grainScheduledService = _grainScheduledService;
-
-        // TODO remove
-        this._listMappedById = {};
     }
 
 
@@ -65,31 +62,11 @@ class SubjectScheduledService implements ISubjectScheduledService {
 
 
     public update = function(subjectScheduled:ISubjectScheduled):ng.IPromise<ISubjectScheduled> {
-        var self = this,
-            deferred = this._$q.defer();
-
-        //TODO remove when using real API
-        setTimeout(function(self, subjectScheduled) {
-            self._listMappedById[subjectScheduled.id] = subjectScheduled;
-            deferred.resolve(subjectScheduled);
-        }, 100, self, subjectScheduled);
-
-        return deferred.promise;
+        throw "Update subject scheduled not implemented";
     };
 
     public remove = function(id:number):ng.IPromise<ISubjectScheduled> {
-        var self = this,
-            deferred = this._$q.defer(),
-            subjectScheduled = this._listMappedById[id];
-
-        //TODO remove when using real API
-        subjectScheduled.is_deleted = true;
-        setTimeout(function(self, subjectScheduled) {
-            delete self._listMappedById[subjectScheduled.id];
-            deferred.resolve(subjectScheduled);
-        }, 100, self, subjectScheduled);
-
-        return deferred.promise;
+        throw "remove subject scheduled not implemented";
     };
 
     public createFromSubject = function(subject:ISubject):ISubjectScheduled {
@@ -131,65 +108,30 @@ class SubjectScheduledService implements ISubjectScheduledService {
     }
 
     public loadSubjectScheduled(){
-        var deferred = this._$q.defer();
-        var self = this;
-        var dataSubjectScheduled = [
-            {
-                "id": "1",
-                "subject_id": "57516f008a8bc277ed1b0168",
-                "owner": "Bettie",
-                "created": "Mon Jun 09 1997 11:46:54 GMT+0000 (UTC)",
-                "title": "California",
-                "description": "Consequat et adipisicing nulla reprehenderit.",
-                "picture": "https://i.ytimg.com/vi/gMadZ5PraLo/hqdefault.jpg",
-                "max_score": 30,
-                "begin_date": "1464968639",
-                "due_date": "1467649548",
-                "estimated_duration": "15",
-                "is_over": false,
-                "is_one_shot_submit": false,
-                "is_deleted": false
-            },
-            {
-                "id": "2",
-                "subject_id": "57516f00f234eb90a7a72a6d",
-                "owner": "Sanchez",
-                "created": "Sat Jan 12 2008 05:55:12 GMT+0000 (UTC)",
-                "title": "Louisiana",
-                "description": "Voluptate laborum commodo sint ex labore do ut aute exercitation proident officia.",
-                "picture": "https://i.ytimg.com/vi/gMadZ5PraLo/hqdefault.jpg",
-                "max_score": 25,
-                "begin_date": "1464968639",
-                "due_date": "1451924748",
-                "estimated_duration": "30",
-                "is_over": false,
-                "is_one_shot_submit": false,
-                "is_deleted": false
-            },
-            {
-                "id": "3",
-                "subject_id": "57516f00b0fba133ba70e20a",
-                "owner": "Burke",
-                "created": "Wed Jan 07 2009 09:51:53 GMT+0000 (UTC)",
-                "title": "Montana",
-                "description": "Dolore id sunt sit elit tempor cillum nisi cillum nulla officia aliquip do ut.",
-                "picture": "https://i.ytimg.com/vi/gMadZ5PraLo/hqdefault.jpg",
-                "max_score": 10,
-                "begin_date": "1464968639",
-                "due_date": "1465057548",
-                "estimated_duration": "60",
-                "is_over": false,
-                "is_one_shot_submit": false,
-                "is_deleted": false
-            }
-        ];
-        setTimeout(function(){
-            angular.forEach(dataSubjectScheduled, function(subjectScheduled){
-                self._listMappedById[subjectScheduled.id] = subjectScheduled;
-            });
-            deferred.resolve(self._listMappedById);
-
-        }, 2000);
+        console.log('loadSubjectScheduled');
+        var self = this,
+            deferred = this._$q.defer(),
+            request = {
+                method: 'GET',
+                url: 'subjects-scheduled'
+            };
+        if (!angular.isUndefined(this._listMappedById)) {
+            deferred.resolve(true);
+        } else {
+            this._$http(request).then(
+                function(response) {
+                    console.log('response', response);
+                    angular.forEach(response, function(subjectScheduled){
+                        self._listMappedById[subjectScheduled.id] = subjectScheduled;
+                    });
+                    deferred.resolve(response);
+                },
+                function(err) {
+                    console.error(err);
+                    deferred.reject('Une erreur est survenue lors de la récupération de vos sujets programmé.');
+                }
+            );
+        }
         return deferred.promise;
     }
 }
