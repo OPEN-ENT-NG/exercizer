f1129d468aa22609a9893ab8588361cb469659bd
Julien Neuhart <j.neuhart@thecodingmachine.com>
2016-06-14 10:15:40 +0200

    wip preview (almost done v2...)

--- src/main/resources/public/app/components/grain/qcm/directives/viewQcm.ts
+++ src/main/resources/public/app/components/grain/qcm/directives/viewQcm.ts
@@ -12,7 +12,7 @@ directives.push(
                 templateUrl: 'exercizer/public/app/components/grain/qcm/templates/view-qcm.html',
                 link:(scope:any) => {
 
-                    if (angular.isUndefined(scope.grainCopy.grain_copy_data.calculated_score)) {
+                    if (angular.isUndefined(scope.grainCopy.calculated_score)) {
                         var result = QcmService.automaticCorrection(scope.grainScheduled, scope.grainCopy);
                         scope.grainCopy.calculated_score = result.calculated_score;
                         scope.isCorrect = result.answers_result;
