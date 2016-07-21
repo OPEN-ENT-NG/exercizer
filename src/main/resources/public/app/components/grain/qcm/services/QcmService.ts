6d6c237de5ae9a14b997f43c2713c8b86cdf4b87
Erwan Le Poder <e.lepoder@thecodingmachine.com>
2016-07-21 11:07:55 +0200

    fix QCM

--- src/main/resources/public/app/components/grain/qcm/services/QcmService.ts
+++ src/main/resources/public/app/components/grain/qcm/services/QcmService.ts
@@ -5,6 +5,7 @@ class QcmService implements IAutomaticCorrection {
     public automaticCorrection(grainScheduled:IGrainScheduled, grainCopy:IGrainCopy):{calculated_score:number, answers_result:{}} {
 
         var numberGoodAnswer : number = 0,
+            numberRecognizedAnswer : number = 0,
             atLeastOneError =  false,
             isCorrectReturnArray = {};
         angular.forEach(grainCopy.grain_copy_data.custom_copy_data.filled_answer_list, function(filled_answer, key){
@@ -15,23 +16,56 @@ class QcmService implements IAutomaticCorrection {
                 grainScheduled.grain_data.custom_data.correct_answer_list[key].isChecked = false;
             }
             if(filled_answer.isChecked === grainScheduled.grain_data.custom_data.correct_answer_list[key].isChecked){
-                isCorrectReturnArray[key] = true;
-                if(grainScheduled.grain_data.custom_data.no_error_allowed && atLeastOneError){
-                    // do not incremented
-                } else{
-                    numberGoodAnswer++
+                if(grainScheduled.grain_data.custom_data.correct_answer_list[key].isChecked){
+                    // response is check and good
+                    // in this case numberGoodAnswer++
+                    numberRecognizedAnswer++;
+                    isCorrectReturnArray[key] = true;
+                    if(grainScheduled.grain_data.custom_data.no_error_allowed && atLeastOneError){
+                        // do not incremented
+                    } else{
+                        numberGoodAnswer++
+                    }
+
+                } else {
+                    // response is not check and good
+                    // in this case the answer is not good
+
                 }
+
             } else {
+
                 isCorrectReturnArray[key] = false;
                 atLeastOneError = true;
+                numberRecognizedAnswer++;
                 if(grainScheduled.grain_data.custom_data.no_error_allowed){
                     numberGoodAnswer = 0;
                 }
+
+                if(grainScheduled.grain_data.custom_data.correct_answer_list[key].isChecked){
+                    // response is check and not good
+                } else {
+                    // response is not check and not good
+
+                }
             }
         });
-        // computation score
-        var numberPossibleAnswer : number = grainScheduled.grain_data.custom_data.correct_answer_list.length;
-        var calculated_score = (numberGoodAnswer / numberPossibleAnswer) * grainScheduled.grain_data.max_score;
+
+        var calculated_score;
+        if(numberRecognizedAnswer == 0){
+            if(atLeastOneError){
+                calculated_score = 0
+            } else{
+                calculated_score = grainScheduled.grain_data.max_score;
+            }
+
+        } else {
+            // computation score
+            calculated_score = (numberGoodAnswer / numberRecognizedAnswer) * grainScheduled.grain_data.max_score;
+        }
+
+
+
         if(isNaN(calculated_score)){
             throw "calculated score is not a number"
         }
