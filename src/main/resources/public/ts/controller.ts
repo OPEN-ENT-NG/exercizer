a432fe585210f27cdbcfa3341759af678a13aa32
Erwan Le Poder <e.lepoder@thecodingmachine.com>
2016-08-30 12:16:20 +0200

    cache angular

--- src/main/resources/public/ts/controller.ts
+++ src/main/resources/public/ts/controller.ts
@@ -143,6 +143,23 @@ function ExercizerController($scope, $rootScope, model, template, route, date, $
 (window as any).AngularExtensions = {
     init: function(module){
 
+        module.config(['$httpProvider', function($httpProvider) {
+            //initialize get if not there
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};
+            }
+
+            // Answer edited to include suggestions from comments
+            // because previous version of code introduced browser-related errors
+
+            //disable IE ajax request caching
+            $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+            // extra
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        }]);
+
+
         /**
          * Filters
          */
