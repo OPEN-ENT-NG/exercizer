5459e15324266893383f842c94e07e38675297aa
Camille Wintz <camille.wintz@web-education.net>
2016-09-02 16:44:09 +0200

    exercizes correction wasn't displayed

--- src/main/resources/public/app/components/grain/filltext/directives/fillzone.ts
+++ src/main/resources/public/app/components/grain/filltext/directives/fillzone.ts
@@ -7,7 +7,7 @@
                     zoneId: '@'
                 },
                 template: '<text-zone ng-class="{ success: optionData.correction && optionData.isCorrect, error: optionData.correction && !optionData.isCorrect }">' +
-                '<div ng-class="{ success: optionData.correction[$index], error: !optionData.correction[$index] }" ng-if="optionData.mode === \'view\'"><span>[[optionData.zone.answer]]</span></div>' +
+                '<div ng-if="optionData.mode === \'view\'"><span>[[optionData.zone.answer]]</span></div>' +
                 '<input type="text" disabled placeholder="[[optionData.zone.answer]]" ng-if="optionData.mode === \'edit\'" />' +
                 '<input type="text" ng-if="optionData.mode === \'perform-text\'" ng-model="optionData.zone.answer" />' +
                 '<select ng-if="optionData.mode === \'perform-list\'" ng-options="o as o for o in optionData.zone.options" ng-model="optionData.zone.answer"></select>' +
@@ -19,6 +19,12 @@
                         zone: _.findWhere(scope.$parent.$eval('customData.zones'), { id: parseInt(scope.zoneId) }),
                         correction: scope.$parent.$eval('correction')
                     }
+
+                    if (scope.optionData.correction) {
+                        let index = scope.$parent.$eval('customData.zones').indexOf(scope.optionData.zone);
+                        scope.optionData.isCorrect = scope.optionData.correction[index];
+                    }
+
                     setTimeout(() => {
                         scope.optionData.mode = 'view';
                         if (element.parents('edit-fill-text').length > 0) {
@@ -35,10 +41,7 @@
                         e.preventDefault();
                         scope.$apply();
                     });
-                    if (scope.correction) {
-                        let index = scope.$parent.$eval('customData.zones').indexOf(scope.zone);
-                        scope.optionData.isCorrect = scope.correction[index];
-                    }
+                    
 
                     scope.answer = ($item) => {
                         scope.$parent.removeAnswer(scope.zone);
