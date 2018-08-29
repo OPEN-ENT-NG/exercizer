4c3ec39c6304cf067e22dfd7b65c64392938cc22
sinthu <sinthujan.sivanathan@gmail.com>
2018-08-29 16:21:03 +0200

    [Ano] #19977 - fix grain refresh

--- src/main/resources/public/ts/app/components/grain/statement/directives/performStatement.ts
+++ src/main/resources/public/ts/app/components/grain/statement/directives/performStatement.ts
@@ -11,15 +11,17 @@ export const performStatement = ng.directive('performStatement',
             },
             templateUrl: 'exercizer/public/ts/app/components/grain/statement/templates/perform-statement.html',
             link:(scope:any) => {
-                if (angular.isUndefined(scope.grainCopy.grain_copy_data.custom_copy_data)) {
-                    scope.grainCopy.grain_copy_data.custom_copy_data = new StatementCustomCopyData();
-                    scope.grainCopy.grain_copy_data.custom_copy_data.statement = '';
-                    scope.statementHtml = $sce.trustAsHtml(scope.grainCopy.grain_copy_data.custom_copy_data.statement);
-                } else if (angular.isUndefined(scope.grainCopy.grain_copy_data.custom_copy_data.statement)) {
-                    scope.statementHtml = '';
-                } else {
-                    scope.statementHtml = $sce.trustAsHtml(scope.grainCopy.grain_copy_data.custom_copy_data.statement);
-                }
+                scope.$watch('grainCopy', function(newValue, oldValue) {
+                    if (angular.isUndefined(scope.grainCopy.grain_copy_data.custom_copy_data)) {
+                        scope.grainCopy.grain_copy_data.custom_copy_data = new StatementCustomCopyData();
+                        scope.grainCopy.grain_copy_data.custom_copy_data.statement = '';
+                        scope.statementHtml = $sce.trustAsHtml(scope.grainCopy.grain_copy_data.custom_copy_data.statement);
+                    } else if (angular.isUndefined(scope.grainCopy.grain_copy_data.custom_copy_data.statement)) {
+                        scope.statementHtml = '';
+                    } else {
+                        scope.statementHtml = $sce.trustAsHtml(scope.grainCopy.grain_copy_data.custom_copy_data.statement);
+                    }
+                });
             }
         };
     }]
