59f1685c7d3f4591525e8f701372edca2a862976
David Breyton <david.breyton@web-education.net>
2019-01-24 14:52:24 +0100

    [Fix] #22054 : remove zero-width characters in answers

--- src/main/resources/public/ts/app/models/helpers/CompareStringHelper.ts
+++ src/main/resources/public/ts/app/models/helpers/CompareStringHelper.ts
@@ -3,9 +3,10 @@ export class CompareStringHelper {
         var result = false;
         
         if (typeof string1 !=='undefined' && typeof string2 !== 'undefined' && string1 && string2) {
-            string1 = string1.replace(/\s/g, '').toLowerCase();
-            string2 = string2.replace(/\s/g, '').toLowerCase();
-            
+            // remove all space and zero-width characters
+            string1 = string1.replace(/[\u200B\u200C\s]/g, '').toLowerCase();
+            string2 = string2.replace(/[\u200B\u200C\s]/g, '').toLowerCase();
+           
             result = string1 === string2;
         }
         
