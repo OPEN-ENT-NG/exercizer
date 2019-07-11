b9f3dbb581e5b88e74da3a865406f7ba16252164
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2019-07-11 20:53:42 +0200

    fix: #25241, fixed exercizer printing template

--- src/main/resources/public/ts/app/controllers/PrintSubjectController.ts
+++ src/main/resources/public/ts/app/controllers/PrintSubjectController.ts
@@ -125,7 +125,7 @@ class PrintSubjectController extends EditSubjectController {
     }
 
     public getGrainType(grain: IGrain) {
-        var type = grain.grain_data.custom_data.answersType;
+        var type = grain.grain_data.custom_data ? grain.grain_data.custom_data.answersType : "";
         return type === "text" ? "exercizer.grain.option1" : type === "list" ? "exercizer.grain.option2" : "exercizer.grain.option3";
     }
 
@@ -134,7 +134,7 @@ class PrintSubjectController extends EditSubjectController {
     }
 
     public getGrainStatement(grain: IGrain) {
-        var statement = grain.grain_data.statement || grain.grain_data.custom_data.statement;
+        var statement = grain.grain_data.statement || (grain.grain_data.custom_data && grain.grain_data.custom_data.statement);
         return this._$sce.trustAsHtml(this.formatHtml(statement));
     }
 
@@ -157,45 +157,47 @@ class PrintSubjectController extends EditSubjectController {
     }
 
     public getCorrectAnswer(grain: IGrain) {
-        return grain.grain_data.custom_data.correct_answer;
+        return grain.grain_data.custom_data && grain.grain_data.custom_data.correct_answer;
     }
 
     public getCorrectAnswers(grain: IGrain) {
-        return grain.grain_data.custom_data.correct_answer_list;
+        return grain.grain_data.custom_data && grain.grain_data.custom_data.correct_answer_list;
     }
 
     public getHtmlContent(grain: IGrain) {
-        var html = grain.grain_data.custom_data.htmlContent;
-        grain.grain_data.custom_data.zones.forEach(answer => {
-            var list = " _______ [";
-            var suffix;
-            var first = true;
-            if (answer.options.length === 0) {
-                list += "<b><u>" + answer.answer + "</u></b>";
-            } else {
-                answer.options.forEach(prop => {
-                    if (answer.answer !== prop) {
-                        suffix = (first ? "" : ", ") + prop;
-                    } else {
-                        suffix = (first ? "" : ", ") + "<b><u>" + prop + "</u></b>";
-                    }
-                    list += suffix;
-                    first = false;
-                });
-            }
-            list += "] ";
-            html = html.replace(new RegExp('<fill-zone([^>]*?)zone-id=\\"'+answer.id+'\\".*?>.*?\\/fill-zone>','gs'), list);
-        });
-        html = html.replace(new RegExp(' &nbsp;','gs')," ")
+        var html = grain.grain_data.custom_data ? grain.grain_data.custom_data.htmlContent : "";
+        if (grain.grain_data.custom_data && grain.grain_data.custom_data.zones) {
+            grain.grain_data.custom_data.zones.forEach(answer => {
+                var list = " _______ [";
+                var suffix;
+                var first = true;
+                if (answer.options.length === 0) {
+                    list += "<b><u>" + answer.answer + "</u></b>";
+                } else {
+                    answer.options.forEach(prop => {
+                        if (answer.answer !== prop) {
+                            suffix = (first ? "" : ", ") + prop;
+                        } else {
+                            suffix = (first ? "" : ", ") + "<b><u>" + prop + "</u></b>";
+                        }
+                        list += suffix;
+                        first = false;
+                    });
+                }
+                list += "] ";
+                html = html.replace(new RegExp('<fill-zone([^>]*?)zone-id=\\"'+answer.id+'\\".*?>.*?\\/fill-zone>','gm'), list);
+            });
+        }
+        html = html.replace(new RegExp(' &nbsp;','gm')," ")
         return this._$sce.trustAsHtml(this.formatHtml(html));
     }
 
     public getZones(grain: IGrain) {
-        return grain.grain_data.custom_data.zones;
+        return grain.grain_data.custom_data && grain.grain_data.custom_data.zones;
     }
 
     public getImage(grain: IGrain) {
-        return grain.grain_data.custom_data._guideImage || skin.basePath + 'img/illustrations/image-default.svg';
+        return (grain.grain_data.custom_data && grain.grain_data.custom_data._guideImage) || skin.basePath + 'img/illustrations/image-default.svg';
     }
 
     public getScheduledDescription() {
@@ -211,6 +213,9 @@ class PrintSubjectController extends EditSubjectController {
     }
 
     private formatHtml(template: string) {
+        if (!template) {
+            return template;
+        }
         template = this.replaceVideo(template);
         template = this.replaceAudio(template);
         template = this.resizeImages(template);
@@ -218,27 +223,27 @@ class PrintSubjectController extends EditSubjectController {
         return template;
     }
 
-    private iframeRegexp = new RegExp('<iframe.*?src=\\"(.+?)[\\?|\\"].*?\\/iframe>','gs');
+    private iframeRegexp = new RegExp('<iframe.*?src=\\"(.+?)[\\?|\\"].*?\\/iframe>','gm');
 
     private replaceVideo(template: string) {
         return template.replace(this.iframeRegexp, "<img src='" + skin.basePath + "img/icons/video-large.png' width='135' height='135'><br><a href=\"$1\">$1</a>");
     }
 
-    private audioRegexp = new RegExp('<div class=\\"audio-wrapper.*?\\/div>','gs');
+    private audioRegexp = new RegExp('<div class=\\"audio-wrapper.*?\\/div>','gm');
 
     private replaceAudio(template: string) {
         return template.replace(this.audioRegexp, "<img src='" + skin.basePath + "img/illustrations/audio-file.png' width='300' height='72'>");
     }
 
-    private imageRegexp1 = new RegExp('(<img[^>]*?class=\\"smiley.*?\\")(.*?>)','gs');
-    private imageRegexp2 = new RegExp('(<span[^>]*?class=\\"image-container.*?>).*?(<img.*?)>.*?<\\/span>','gs');
+    private imageRegexp1 = new RegExp('(<img[^>]*?class=\\"smiley.*?\\")(.*?>)','gm');
+    private imageRegexp2 = new RegExp('(<span[^>]*?class=\\"image-container.*?>).*?(<img.*?)>.*?<\\/span>','gm');
 
     private resizeImages(template: string) {
         return template.replace(this.imageRegexp1, "$1" + " style=\"height: 40px; width: 40px;\" " + "$2")
         .replace(this.imageRegexp2, "$2 style=\"max-width:300px;\">");
     }
 
-    private hrefRegexp = new RegExp('<a.*?href=[\\"|\'](.+?)[\\"|\'].*?>(.*?)<\\/a>','gs');
+    private hrefRegexp = new RegExp('<a.*?href=[\\"|\'](.+?)[\\"|\'].*?>(.*?)<\\/a>','gm');
 
     private replaceHref(template: string) {
         return template.replace(this.hrefRegexp, "$2\[<a href=\"$1\">$1</a>\] ");
