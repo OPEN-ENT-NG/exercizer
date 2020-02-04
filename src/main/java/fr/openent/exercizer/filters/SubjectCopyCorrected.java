200f07dbc0200fc183a3b733480fbcd6d3248023
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2020-02-04 09:57:46 +0100

    fix: #30447, teachers should be allow to submit copies

--- src/main/java/fr/openent/exercizer/filters/SubjectCopyCorrected.java
+++ src/main/java/fr/openent/exercizer/filters/SubjectCopyCorrected.java
@@ -40,8 +40,9 @@ public class SubjectCopyCorrected implements ResourcesProvider {
 
                 String query = "SELECT COUNT(sc.id) FROM " +
                         conf.getSchema() + "subject_scheduled as ss INNER JOIN " + conf.getSchema() + "subject_copy sc ON ss.id = sc.subject_scheduled_id " +
-                        "WHERE "+("Student".equalsIgnoreCase(user.getType()) ? "sc.owner = ?" : "ss.owner = ?")+" AND sc.id = ? ";
+                        "WHERE (sc.owner = ? OR ss.owner = ?) AND sc.id = ? ";
                 JsonArray values = new fr.wseduc.webutils.collections.JsonArray();
+                values.add(user.getUserId());
                 values.add(user.getUserId());
                 values.add(Sql.parseId(id));
 
