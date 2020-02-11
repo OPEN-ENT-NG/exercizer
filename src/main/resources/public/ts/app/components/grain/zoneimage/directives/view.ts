9e72fee7a1e5a6bcce156d654eed79267bdc7581
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2020-02-11 17:15:48 +0100

    fix: #29025, fixed correction button's conditional appearance

--- src/main/resources/public/ts/app/components/grain/zoneimage/directives/view.ts
+++ src/main/resources/public/ts/app/components/grain/zoneimage/directives/view.ts
@@ -23,7 +23,8 @@ export const viewZoneImage = ng.directive('viewZoneImage',
                 }
                 scope.displayCorrectAnswerButton = function() {
                     return !scope.isTeacher && !angular.isUndefined(scope.grainCopy.final_score)
-                    && !angular.isUndefined(scope.displayCorrection) && (scope.grainCopy.final_score != scope.grainCopy.grain_copy_data.max_score);
+                    && !angular.isUndefined(scope.displayCorrection) && ((scope.grainCopy.final_score == null) || 
+                    (scope.grainCopy.final_score < scope.grainCopy.grain_copy_data.max_score));
                 };
 
                 var result = automaticCorrection(scope.grainScheduled, scope.grainCopy);
