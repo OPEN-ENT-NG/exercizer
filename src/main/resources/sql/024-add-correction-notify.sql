e694cae6ef0240add70e5308f6ed5bc5f8f71620
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2020-05-01 10:19:52 +0200

    fix: #34742, set correction_notify to true for all pre-existing subjects

--- src/main/resources/sql/024-add-correction-notify.sql
+++ src/main/resources/sql/024-add-correction-notify.sql
@@ -1,1 +1,6 @@
 ALTER TABLE exercizer.subject_scheduled ADD COLUMN correction_notify BOOLEAN DEFAULT FALSE;
+
+
+/* We must consider pre-existing subjects as already notified */
+
+UPDATE exercizer.subject_scheduled SET correction_notify = TRUE;
