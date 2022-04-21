03cda01058bae7e3a7e0b9f14deec61c67f75e7c
Clément Creusat <clement.creusat@gmail.com>
2022-04-21 13:03:13 +0200

    feat: #WB-451 qcm and directive auto expand

---
+++ src/main/resources/public/ts/app/directives/autoexpand/autoExpand.ts
@@ -0,0 +1,38 @@
+import { ng } from 'entcore';
+
+export const autoExpand = ng.directive('autoExpand',
+	[function () {
+		return {
+			restrict: "EA",
+			link: function(scope, element, attr){
+                let messageLastScrollHeight = element[0].scrollHeight;
+                element[0].addEventListener('keydown', handleAutoExand);
+                element[0].addEventListener('focus', () => {
+                    let calc = element[0].scrollHeight / 16;
+                    
+                    if (element[0].value === '') element[0].setAttribute("rows", "1");
+    
+                    element[0].setAttribute('rows', parseInt(String(calc)));
+                }, { once: true })
+
+                function handleAutoExand () {
+                    let rows = parseInt(element[0].getAttribute("rows"));
+                    element[0].setAttribute("rows", "1");
+                    // element[0].style.height = 'auto';
+                    if (element[0].value === '') {
+                        rows = 1;
+                    }
+                    
+                    if (element[0].scrollHeight > messageLastScrollHeight) {
+                        rows++;
+                    } else if (rows > 1 && element[0].scrollHeight < messageLastScrollHeight) {
+                        rows--;
+                    }
+                    
+                    messageLastScrollHeight = element[0].scrollHeight;
+                    element[0].setAttribute("rows", rows);
+                };
+            }
+		}
+	}]
+);
