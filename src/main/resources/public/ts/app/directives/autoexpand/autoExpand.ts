82e4c9b1941b7e569008d6e23a785bf723351e73
Clément Creusat <clement.creusat@gmail.com>
2022-04-25 16:59:47 +0200

    feat: #WB-533 zone edit

--- src/main/resources/public/ts/app/directives/autoexpand/autoExpand.ts
+++ src/main/resources/public/ts/app/directives/autoexpand/autoExpand.ts
@@ -6,6 +6,9 @@ export const autoExpand = ng.directive('autoExpand',
 			restrict: "EA",
 			link: function(scope, element, attr){
                 let messageLastScrollHeight = element[0].scrollHeight;
+
+                element[0].setAttribute("rows", "1");
+                
                 element[0].addEventListener('keydown', handleAutoExand);
                 element[0].addEventListener('focus', () => {
                     let calc = element[0].scrollHeight / 16;
@@ -20,7 +23,6 @@ export const autoExpand = ng.directive('autoExpand',
                 function handleAutoExand () {
                     let rows = parseInt(element[0].getAttribute("rows"));
                     element[0].setAttribute("rows", "1");
-                    // element[0].style.height = 'auto';
                     if (element[0].value === '') {
                         rows = 1;
                     }
