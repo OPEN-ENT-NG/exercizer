ea472027f41e30e4c35cfccce7b3ca1d3f06b25f
david-cc <david.caudeli@web-education.net>
2022-06-16 18:09:26 +0200

    fix(ui): #WB-740, fix message-info margin in interactive subject rendu confirmation

--- src/main/resources/public/ts/app/directives/autoexpand/autoExpand.ts
+++ src/main/resources/public/ts/app/directives/autoexpand/autoExpand.ts
@@ -4,39 +4,31 @@ export const autoExpand = ng.directive('autoExpand',
 	[function () {
 		return {
 			restrict: "EA",
-			link: function(scope, element, attr){
-                let messageLastScrollHeight = element[0].scrollHeight;
-
-                element[0].setAttribute("rows", "1");
-                
-                element[0].addEventListener('keydown', handleAutoExand);
-                element[0].addEventListener('focus', () => {
-                    let calc = element[0].scrollHeight / 16;
-                    
-                    if (element[0].value.length === 0) {
-                        element[0].setAttribute("rows", "1");
-                    } else {
-                        element[0].setAttribute('rows', parseInt(String(calc)));
-                    }
-                }, { once: true })
-
-                function handleAutoExand () {
-                    let rows = parseInt(element[0].getAttribute("rows"));
-                    element[0].setAttribute("rows", "1");
+			link: function(scope, element, attributes){
+                function handleInputDimension() {
                     if (element[0].value === '') {
-                        console.log('empty');
-                        rows = 1;
+                        if (attributes.vertical === "true") {
+                            element[0].style.width = 150 + "px";
+                        } else {
+                            element[0].style.height = 50 + "px";
+                        }
                     }
-                    
-                    if (element[0].scrollHeight > messageLastScrollHeight) {
-                        rows++;
-                    } else if (rows > 1 && element[0].scrollHeight < messageLastScrollHeight) {
-                        rows--;
+                }
+
+                function onInputChange() {
+                    if (attributes.vertical === "true") {
+                        element[0].style.width = "";
+                        element[0].style.width = element[0].scrollWidth + "px";
+                    } else {
+                        element[0].style.height = "";
+                        element[0].style.height = Math.min(element[0].scrollHeight, 300) + "px";
                     }
-                    
-                    messageLastScrollHeight = element[0].scrollHeight;
-                    element[0].setAttribute("rows", rows);
-                };
+                    handleInputDimension();
+                }
+
+                handleInputDimension();
+
+                element[0].addEventListener('input', onInputChange);
             }
 		}
 	}]
