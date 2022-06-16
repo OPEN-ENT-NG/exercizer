bbe414024d009d099f9a4d43c93033837a5f9317
david-cc <david.caudeli@web-education.net>
2022-06-16 18:09:26 +0200

    fix(ui): #WB-740, fix message-info margin in interactive subject rendu confirmation

--- src/main/resources/public/ts/app/components/grain/filltext/directives/fillzone.ts
+++ src/main/resources/public/ts/app/components/grain/filltext/directives/fillzone.ts
@@ -14,7 +14,7 @@ export const fillZone = ng.directive('fillZone',
             '<svg ng-if="optionData.mode === \'edit\'" class="icon-edit" width="18" height="18"><use href="exercizer/public/assets/icons/icons.svg#edit"></use></svg>' +
             '<div ng-if="optionData.mode === \'view\'"><div>[[optionData.zone.answer]]</div></div>' +
             '<div ng-if="optionData.mode === \'edit\'"><span>[[optionData.zone.answer]]</span><input type="hidden" disabled placeholder="[[optionData.zone.answer]]" ng-if="optionData.mode === \'edit\'" /></div>' +
-            '<div ng-if="optionData.mode === \'perform-text\'" style="min-width:150px;height:20px;"><span>[[optionData.zone.answer]]</span><input class="edit-text-zone" maxlength="40" ng-model="optionData.zone.answer" type="text" placeholder="[[optionData.zone.answer]]" ng-if="optionData.mode === \'perform-text\'" /></div>' +
+            '<div ng-if="optionData.mode === \'perform-text\'" style="min-width:150px;height:20px;"><input auto-expand vertical="true" class="edit-text-zone" maxlength="40" ng-model="optionData.zone.answer" type="text" placeholder="[[optionData.zone.answer]]" ng-if="optionData.mode === \'perform-text\'" /></div>' +
             '<select ng-if="optionData.mode === \'perform-list\'" ng-options="o as o for o in optionData.zone.options" ng-model="optionData.zone.answer"></select>' +
             '<div class="no-responsive" drag-item="optionData.zone" drop-item="answer($item)" ng-if="optionData.mode === \'perform-drag\'"><div>[[optionData.zone.answer]]</div></div>' +
             '<div class="no-desktop" drag-item="optionData.zone" drop-item="answer($item)" ng-click="showAnswers($event)" ng-if="optionData.mode === \'perform-drag\'"><div>[[optionData.zone.answer]]</div></div>' +
