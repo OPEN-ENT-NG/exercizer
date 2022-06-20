82cf1710b9c4f145a9db91ac66d8103c4a444a34
Clément Creusat <clement.creusat@gmail.com>
2022-06-20 19:48:09 +0200

    fix(exercizer): feedbacks UI 1D

--- src/main/resources/public/ts/app/components/grain/qcm/directives/performQcm.ts
+++ src/main/resources/public/ts/app/components/grain/qcm/directives/performQcm.ts
@@ -17,8 +17,6 @@ export const performQcm = ng.directive('performQcm',
                     scope.filteredAnswersList = scope.filledAnswerList.filter(answer => answer.text !== "")
                 });
 
-                console.log('qcm', scope.grainCopy.grain_copy_data.custom_copy_data.filled_answer_list);
-
                 scope.updateGrainCopy = function() {
                     scope.$emit('E_UPDATE_GRAIN_COPY', scope.grainCopy);
                 };
