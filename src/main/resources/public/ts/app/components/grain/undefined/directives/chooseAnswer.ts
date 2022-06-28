84b40a9e520783c3539bf57deefd7b625d9106b2
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-06-28 11:14:38 +0200

    feat: #WB-774, reorder questions

--- src/main/resources/public/ts/app/components/grain/undefined/directives/chooseAnswer.ts
+++ src/main/resources/public/ts/app/components/grain/undefined/directives/chooseAnswer.ts
@@ -23,6 +23,24 @@ export const chooseAnswer = ng.directive('chooseAnswer',
                     scope.grain.grain_data.title = '';
                     scope.$emit('E_UPDATE_GRAIN', scope.grain);
                 }
+
+                const preferedGrainOrder = {
+                    1: /*choose*/               10,
+                    2: /*choose-answer*/        20,
+                    7: /*edit-qcm*/             30,
+                    4: /*edit-simple-answer*/   40,
+                    6: /*edit-multiple-answer*/ 50,
+                    5: /*edit-open-answer*/     60,
+                    8: /*edit-association*/     70,
+                    9: /*edit-order*/           80,
+                    10: /*edit-fill-text*/      90,
+                    11: /*edit-zone-text*/      100,
+                    12: /*edit-zone-image*/     110,
+                    3: /*edit-statement*/       120,
+                }
+                scope.grainSortOrder = function(grainTypeId1:{value:number}, grainTypeId2:{value:number}) {
+                    return preferedGrainOrder[grainTypeId1.value] - preferedGrainOrder[grainTypeId2.value];
+                }
             }
         };
     }]
