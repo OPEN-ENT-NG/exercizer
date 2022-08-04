b09597a635a0f318a88c0ad564d20ac42b71a749
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-08-04 17:59:51 +0200

    fix: #WB-896, migration script 29 is failing on subject_schedule having lots of corrected files.

--- src/main/resources/sql/029-simple-subject-files-migration.sql
+++ src/main/resources/sql/029-simple-subject-files-migration.sql
@@ -10,12 +10,10 @@
  - table "subject" does not have the columns "corrected_file_id" and "corrected_metadata" anymore.
  - table "subject_scheduled" does not have the columns "corrected_file_id" and "corrected_metadata" anymore.
  - table "subject_document" may have 1 new row with data copied from "corrected_*" columns.
-   In that case, "subject_document.doc_type" will be set to 'storage' because the document is available from storage directly.
- => Only migrated documents have a 'storage' doc_type.
-    All post-migration documents will be created with a 'workspace' doc_type.
+ - Files indexed in table "subject_document" have doc_type 'storage'. Type 'workspace' is reserved for future use.
 
  - table "subject_copy" does not have the columns "corrected_file_id", "corrected_metadata", "homework_file_id" and "homework_metadata" anymore.
- - table "subject_copy_file" may have 2 new rows with data copied from "corrected_*" and "homework_*" columns.
+ - table "subject_copy_file" may have 2 new rows with data copied from "corrected_*" and "homework_*" columns
    with "file_type" set to 'corrected' or 'homework' accordingly.
  => Files indexed in table "subject_copy_file" are always available from Storage (not Workspace)
 */
@@ -24,7 +22,6 @@ CREATE OR REPLACE FUNCTION exercizer.migrate_subject_files(int) RETURNS SETOF bi
 $$
 DECLARE
   batch_size ALIAS FOR $1;
-  apply_offset int := 0;
   last_insert_count int;
 BEGIN
   LOOP
@@ -35,9 +32,9 @@ BEGIN
       WHERE s.corrected_file_id IS NOT NULL AND s.corrected_metadata IS NOT NULL
         AND sd.subject_id IS NULL AND sd.doc_id IS NULL  -- avoid duplicating already migrated files
       ORDER BY s.id
-      LIMIT batch_size OFFSET apply_offset;
+      LIMIT batch_size;
     GET DIAGNOSTICS last_insert_count := ROW_COUNT;
-    apply_offset := apply_offset + last_insert_count;
+    RAISE NOTICE 'Migrated % files from exercizer.subject to exercizer.subject_document', last_insert_count;
     EXIT WHEN (last_insert_count < batch_size);
   END LOOP;
 
@@ -61,7 +58,6 @@ CREATE OR REPLACE FUNCTION exercizer.migrate_subject_scheduled_files(int) RETURN
 $$
 DECLARE
   batch_size ALIAS FOR $1;
-  apply_offset int := 0;
   last_insert_count int;
 BEGIN
   LOOP
@@ -71,10 +67,10 @@ BEGIN
       LEFT JOIN exercizer.subject_document sd ON sd.subject_id = ss.subject_id AND sd.doc_id = ss.corrected_file_id AND sd.doc_type = 'storage'
       WHERE ss.corrected_file_id IS NOT NULL AND ss.corrected_metadata IS NOT NULL
         AND sd.subject_id IS NULL AND sd.doc_id IS NULL  -- avoid duplicating already migrated files
-      ORDER BY ss.subject_id
-      LIMIT batch_size OFFSET apply_offset;
+      ORDER BY ss.id
+      LIMIT batch_size;
     GET DIAGNOSTICS last_insert_count := ROW_COUNT;
-    apply_offset := apply_offset + last_insert_count;
+    RAISE NOTICE 'Migrated % files from exercizer.subject_scheduled to exercizer.subject_document', last_insert_count;
     EXIT WHEN (last_insert_count < batch_size);
   END LOOP;
 
@@ -98,7 +94,6 @@ CREATE OR REPLACE FUNCTION exercizer.migrate_subject_copy_corrected_files(int) R
 $$
 DECLARE
   batch_size ALIAS FOR $1;
-  apply_offset int := 0;
   last_insert_count int;
 BEGIN
   LOOP
@@ -109,9 +104,9 @@ BEGIN
       WHERE sc.corrected_file_id IS NOT NULL AND sc.corrected_metadata IS NOT NULL
         AND scf.subject_copy_id IS NULL AND scf.file_id IS NULL  -- avoid duplicating already migrated files
       ORDER BY sc.id
-      LIMIT batch_size OFFSET apply_offset;
+      LIMIT batch_size;
     GET DIAGNOSTICS last_insert_count := ROW_COUNT;
-    apply_offset := apply_offset + last_insert_count;
+    RAISE NOTICE 'Migrated % corrected files from exercizer.subject_copy to exercizer.subject_copy_file', last_insert_count;
     EXIT WHEN (last_insert_count < batch_size);
   END LOOP;
 
@@ -135,7 +130,6 @@ CREATE OR REPLACE FUNCTION exercizer.migrate_subject_copy_homework_files(int) RE
 $$
 DECLARE
   batch_size ALIAS FOR $1;
-  apply_offset int := 0;
   last_insert_count int;
 BEGIN
   LOOP
@@ -146,9 +140,9 @@ BEGIN
       WHERE sc.homework_file_id IS NOT NULL AND sc.homework_metadata IS NOT NULL
         AND scf.subject_copy_id IS NULL AND scf.file_id IS NULL  -- avoid duplicating already migrated files
       ORDER BY sc.id
-      LIMIT batch_size OFFSET apply_offset;
+      LIMIT batch_size;
     GET DIAGNOSTICS last_insert_count := ROW_COUNT;
-    apply_offset := apply_offset + last_insert_count;
+    RAISE NOTICE 'Migrated % homework files from exercizer.subject_copy to exercizer.subject_copy_file', last_insert_count;
     EXIT WHEN (last_insert_count < batch_size);
   END LOOP;
 
@@ -168,8 +162,39 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+/* Ensure the limit of 5 corrected files per subject_document will not be reached. */
+-- For every subject_scheduled, detect corrected files doublons.
+update exercizer.subject_scheduled ss3 set corrected_file_id=ss2.dedoublon_id
+from (
+    select ss1.subject_id, ss1.id, ss1.corrected_file_id, MAX(doublons.corrected_file_id) as dedoublon_id
+    from exercizer.subject_scheduled ss1 
+    left join (
+        select distinct ss1.subject_id, ss1.id as subject_scheduled_id, ss1.corrected_file_id, corrected_metadata->>'filename' as corrected_filename 
+        from exercizer.subject_scheduled ss1 
+        where ss1.corrected_file_id is not null 
+        and ss1.subject_id in (
+            select subject_id from exercizer.subject_scheduled where corrected_file_id is not null group by subject_id having count(corrected_file_id) > 5
+        )
+    ) as doublons on doublons.subject_id = ss1.subject_id and doublons.corrected_filename = ss1.corrected_metadata->>'filename'
+    where ss1.corrected_file_id is not null 
+    group by ss1.subject_id, ss1.id, ss1.corrected_file_id having count(ss1.corrected_file_id) > 1
+    order by ss1.subject_id
+) as ss2
+where ss3.id = ss2.id;
+-- Keep only first doublon
+update exercizer.subject_scheduled ss set corrected_file_id=null
+from (
+    select ss1.subject_id, ss1.corrected_file_id, MIN(ss1.id) as id
+    from exercizer.subject_scheduled ss1 
+    where ss1.corrected_file_id is not null 
+    group by ss1.subject_id, ss1.corrected_file_id having count(ss1.id) > 1 
+    order by ss1.subject_id 
+) as ss2 
+where ss2.subject_id = ss.subject_id and ss2.corrected_file_id = ss.corrected_file_id and ss2.id <> ss.id;
+
+
 /* Do migrate files */
 SELECT * FROM exercizer.migrate_subject_files(300);
 SELECT * FROM exercizer.migrate_subject_scheduled_files(300);
 SELECT * FROM exercizer.migrate_subject_copy_corrected_files(300);
-SELECT * FROM exercizer.migrate_subject_copy_homework_files(300);
+SELECT * FROM exercizer.migrate_subject_copy_homework_files(1000);
