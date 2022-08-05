dd8ce5f9c1afb8aafc17e3b7502d8c2b0bcc650b
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2022-08-05 16:01:00 +0200

    fix: import didn't work with old archives

--- src/main/java/fr/openent/exercizer/events/ExercizerRepositoryEvents.java
+++ src/main/java/fr/openent/exercizer/events/ExercizerRepositoryEvents.java
@@ -23,6 +23,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
+import java.io.File;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -236,6 +237,155 @@ public class ExercizerRepositoryEvents extends SqlRepositoryEvents {
         });
 
     }
+
+    // TO DO: Remove this override when automatic archive mogration is implemented
+    @Override
+    public void importTables(String importPath, String schema, List<String> tables, Map<String,String> tablesWithId,
+                                String userId, String username, String locale, SqlStatementsBuilder builder, boolean forceImportAsDuplication,
+                                Handler<JsonObject> handler, Map<String, JsonObject> idsMapByTable, String duplicateSuffix) {
+        if (tables.isEmpty()) {
+            tablesWithId.keySet().forEach(table -> {
+                if (tablesWithId.get(table).equals("DEFAULT")) {
+                    builder.raw("UPDATE " + schema + "." + table + " AS mytable " +
+                            "SET id = DEFAULT WHERE " +
+                            "(SELECT (CASE WHEN mytable.id > (SELECT (CASE WHEN is_called THEN last_value ELSE 0 END)" +
+                            " FROM " + schema + "." + table + "_id_seq) THEN TRUE " +
+                            "ELSE FALSE END))");
+                }
+            });
+            JsonArray statements = builder.build();
+            importDocumentsDependancies(importPath, userId, username, statements, done -> {
+                sql.transaction(done, message -> {
+                    int resourcesNumber = 0, duplicatesNumber = 0, errorsNumber = 0;
+                    if ("ok".equals(message.body().getString("status")))
+                    {
+                        JsonArray results = message.body().getJsonArray("results");
+                        Map<String, Integer> dupsMap = new HashMap<String, Integer>();
+
+                        for (int i = 0; i < results.size(); i++)
+                        {
+                            JsonObject jo = results.getJsonObject(i);
+
+                            if (!"ok".equals(jo.getString("status"))) {
+                                errorsNumber++;
+                            } else {
+                                if (jo.getJsonArray("fields").contains("duplicates"))
+                                {
+                                    String collec = jo.getJsonArray("results").getJsonArray(0).getString(0);
+                                    Integer dups = jo.getJsonArray("results").getJsonArray(0).getInteger(1);
+                                    Integer oldDups = dupsMap.getOrDefault(collec, 0);
+
+                                    dupsMap.put(collec, oldDups + dups);
+                                    duplicatesNumber += dups;
+                                }
+                                if (jo.getJsonArray("fields").contains("noduplicates")) {
+                                    resourcesNumber += jo.getJsonArray("results").getJsonArray(0).getInteger(1);
+                                }
+                            }
+                        }
+
+                        JsonObject finalMap = new JsonObject();
+                        for(Map.Entry<String, JsonObject> entry : idsMapByTable.entrySet())
+                            finalMap.put(entry.getKey(), entry.getValue());
+
+                        JsonObject reply =
+                            new JsonObject()
+                                .put("status","ok")
+                                .put("resourcesNumber", String.valueOf(resourcesNumber))
+                                .put("errorsNumber", String.valueOf(errorsNumber))
+                                .put("duplicatesNumber", String.valueOf(duplicatesNumber))
+                                .put("resourcesIdsMap", finalMap)
+                                .put("duplicatesNumberMap", dupsMap)
+                                .put("mainResourceName", mainResourceName);
+
+                        log.info(title + " : Imported "+ resourcesNumber + " resources (" + duplicatesNumber + " duplicates) with " + errorsNumber + " errors." );
+                        handler.handle(reply);
+                    } else {
+                        log.error(title + " Import error: " + message.body().getString("message"));
+                        handler.handle(new JsonObject().put("status", "error"));
+                    }
+
+                });
+            });
+        } else {
+            String table = tables.remove(0);
+            String path = importPath + File.separator + schema + "." + table;
+            fs.readFile(path, result -> {
+                if (result.failed()) {
+                    if ("subject_document".equals(table)) {
+                        // Prevent error if archive doesn't contain "subject.document" table
+                        importTables(importPath, schema, tables, tablesWithId, userId, username, locale, builder,
+                            forceImportAsDuplication, handler, idsMapByTable, duplicateSuffix);
+                    } else {
+                        log.error(title + " : Failed to read table "+ schema + "." + table + " in archive.");
+                        handler.handle(new JsonObject().put("status", "error"));
+                    }
+                } else {
+                    JsonObject tableContent = result.result().toJsonObject();
+                    JsonArray fields = tableContent.getJsonArray("fields");
+                    JsonArray results = tableContent.getJsonArray("results");
+
+                    int idIx = 0;
+                    for(int i = 0; i < fields.size(); ++i)
+                    {
+                        if(fields.getString(i).equals("id"))
+                        {
+                            idIx = i;
+                            break;
+                        }
+                    }
+
+                    Map<String, Object> oldIdsToNewIdsMap = new HashMap<String, Object>();
+                    for(int i = results.size(); i-- > 0;)
+                    {
+                        String id = "";
+                        try
+                        {
+                            id = results.getJsonArray(i).getString(idIx);
+                        }
+                        catch(ClassCastException e)
+                        {
+                            id = results.getJsonArray(i).getInteger(idIx).toString();
+                        }
+                        oldIdsToNewIdsMap.put(id, id);
+                    }
+
+                    idsMapByTable.put(table, new JsonObject(oldIdsToNewIdsMap));
+
+                    if (!results.isEmpty()) {
+                        final JsonArray finalResults = transformResults(fields, results, userId, username, builder, table, forceImportAsDuplication, duplicateSuffix);
+
+                        beforeImportingResultsToTable(importPath, table, fields, finalResults)
+                        .onComplete( r -> {
+                            String insert = "WITH rows AS (INSERT INTO " + schema + "." + table + " (" + String.join(",",
+                                    ((List<String>) fields.getList()).stream().map(f -> "\"" + f + "\"").toArray(String[]::new)) + ") VALUES ";
+                            String conflictUpdate = "ON CONFLICT(id) DO UPDATE SET id = ";
+                            String conflictNothing = "ON CONFLICT DO NOTHING RETURNING 1) SELECT '" + table + "' AS table, "
+                                                    + "count(*) AS " + (tablesWithId.containsKey(table) ? "duplicates" : "noduplicates") + " FROM rows";
+
+                            for (int i = 0; i < finalResults.size(); i++) {
+                                JsonArray entry = finalResults.getJsonArray(i);
+                                String query = insert + Sql.listPrepared(entry);
+
+                                if (tablesWithId.containsKey(table)) {
+                                    builder.prepared(query + conflictUpdate + tablesWithId.get(table) +
+                                            " RETURNING 1) SELECT '" + table + "' AS table, count(*) AS noduplicates FROM rows", entry);
+                                }
+                                builder.prepared(query + conflictNothing, entry);
+                            }
+
+                            importTables(importPath, schema, tables, tablesWithId, userId, username, locale, builder,
+                                forceImportAsDuplication, handler, idsMapByTable, duplicateSuffix);
+                        });
+                    } else {
+                        importTables(importPath, schema, tables, tablesWithId, userId, username, locale, builder,
+                            forceImportAsDuplication, handler, idsMapByTable, duplicateSuffix);
+                    }
+                }
+            });
+        }
+    }
+    //
 
     @Override
     protected Future<Void> beforeImportingResultsToTable(String importPath, String table, final JsonArray fields, final JsonArray rows) {
