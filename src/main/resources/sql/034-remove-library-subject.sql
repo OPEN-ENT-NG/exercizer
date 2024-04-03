a9b94ee1fbefa41474740d5bac4a3beb41857809
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2024-04-03 15:04:02 +0200

    fix: #WB2-539, clean library subjects

--- src/main/resources/sql/034-remove-library-subject.sql
+++ src/main/resources/sql/034-remove-library-subject.sql
@@ -2,6 +2,8 @@ CREATE INDEX exercizer_grain_subjectid_idx ON exercizer.grain USING btree (subje
 CREATE INDEX exercizer_subject_library_main_information_subjectid_idx ON exercizer.subject_library_main_information USING btree (subject_id);
 CREATE INDEX exercizer_subject_library_tag_subjectid_idx ON exercizer.subject_library_tag USING btree (subject_id);
 CREATE INDEX exercizer_subject_islibrary_idx ON exercizer.subject USING btree (is_library_subject);
+DELETE FROM exercizer.subject_document WHERE subject_id IN (SELECT id FROM exercizer.subject WHERE is_library_subject IS TRUE);
+DELETE FROM exercizer.subject_scheduled WHERE subject_id IN (SELECT id FROM exercizer.subject WHERE is_library_subject IS TRUE);
 DELETE FROM exercizer.grain WHERE subject_id IN (SELECT id FROM exercizer.subject WHERE is_library_subject IS TRUE);
 DELETE FROM exercizer.subject_library_main_information WHERE subject_id IN (SELECT id FROM exercizer.subject WHERE is_library_subject IS TRUE);
 DELETE FROM exercizer.subject_library_tag WHERE subject_id IN (SELECT id FROM exercizer.subject WHERE is_library_subject IS TRUE);
