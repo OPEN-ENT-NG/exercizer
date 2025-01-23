af853fe3fe90b7b323d4271ffa8a0f116de35b3c
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2025-01-23 15:51:06 +0100

    fix: #WB2-1700, add ng blur to execute function before leave page

--- src/main/resources/public/ts/app/components/subject/subject_view_copy/directives/subjectViewCopyTeacherActions.ts
+++ src/main/resources/public/ts/app/components/subject/subject_view_copy/directives/subjectViewCopyTeacherActions.ts
@@ -46,17 +46,12 @@ export const subjectViewCopyTeacherActions = ng.directive('subjectViewCopyTeache
                 
                 scope.redirectToDashboard = function(isCorrected:boolean) {
                     if (isCorrected) {
-                        scope.updateGrainCopy();
-                        $timeout(() => {
-                            scope.$applyAsync(() => {
-                                var copy = SubjectCopyService.getById(scope.subjectCopy.id);
-                                copy.is_correction_on_going = true;
-                                copy.is_corrected = true;
-                                copiesYetToCorrect--;
-                                setCopiesYetToCorrectDisplay();
-                                scope.$emit('E_UPDATE_SUBJECT_COPY', copy, false);
-                            });
-                        });
+                        var copy = SubjectCopyService.getById(scope.subjectCopy.id);
+                        copy.is_correction_on_going = true;
+                        copy.is_corrected = true;
+                        copiesYetToCorrect--;
+                        setCopiesYetToCorrectDisplay();
+                        scope.$emit('E_UPDATE_SUBJECT_COPY', copy, false);
                     } else {
                         $location.path('/dashboard/teacher/correction/'+scope.subjectScheduled.id);
                     }
